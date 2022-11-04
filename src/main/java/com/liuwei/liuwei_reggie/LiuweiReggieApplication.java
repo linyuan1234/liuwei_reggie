fff3b65fb78efc9b90eca5ac04544c95bba48950
nantian <1308083921@qq.com>
2022-11-04 10:58:32 +0800

    Initial commit

---
+++ src/main/java/com/liuwei/liuwei_reggie/LiuweiReggieApplication.java
@@ -0,0 +1,13 @@
+package com.liuwei.liuwei_reggie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuweiReggieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiuweiReggieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liuwei/liuwei_reggie/LiuweiReggieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuwei.liuwei_reggie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiuweiReggieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
